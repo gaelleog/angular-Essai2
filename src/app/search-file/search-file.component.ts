d00eb4fcfdc807da3c0d0e1eb5c7c776c7220046
gaelleog <gaelle.auge95@gmail.com>
2021-01-14 14:50:16 +0100

    1 file modified

--- src/app/search-file/search-file.component.ts
+++ src/app/search-file/search-file.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { ApiService } from "../api.service";
 
 @Component({
@@ -8,7 +9,7 @@ import { ApiService } from "../api.service";
   styleUrls: ["./search-file.component.css"]
 })
 export class SearchFileComponent implements OnInit {
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private router: Router) {}
 
   articles: any;
 
@@ -18,8 +19,9 @@ export class SearchFileComponent implements OnInit {
     const uid = form.value["uid"];
     console.log(uid);
     this.apiService.getArticles(uid).subscribe(data => {
-      this.articles = data;
-      console.log(data);
+    this.articles = data;
+    console.log(data);
+    // this.router.navigate(['/articles']);
     });
   }
 }
